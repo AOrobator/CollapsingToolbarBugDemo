2fb5c47544036a174ff72c14a5e6ebed8a32b31f
AndrewOrobator <Orobator.Andrew@gmail.com>
2016-07-25 18:09:01 -0400

    Formatting

--- app/src/main/java/com/orobator/bugs/collapsingtoolbarbugdemo/MainActivity.java
+++ app/src/main/java/com/orobator/bugs/collapsingtoolbarbugdemo/MainActivity.java
@@ -10,67 +10,55 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.recycler_view)
-    RecyclerView recyclerView;
+  @BindView(R.id.recycler_view) RecyclerView recyclerView;
 
-    @BindView(R.id.appbar)
-    AppBarLayout appBarLayout;
+  @BindView(R.id.appbar) AppBarLayout appBarLayout;
 
-    @BindView(R.id.collapsingToolbarLayout)
-    CollapsingToolbarLayout collapsingToolbarLayout;
+  @BindView(R.id.collapsingToolbarLayout) CollapsingToolbarLayout collapsingToolbarLayout;
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
+  @BindView(R.id.toolbar) Toolbar toolbar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
-        collapsingToolbarLayout.setTitleEnabled(true);
+    collapsingToolbarLayout.setTitleEnabled(true);
 
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Now you see me...");
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setTitle("Now you see me...");
 
-        recyclerView.setAdapter(new MyAdapter());
+    recyclerView.setAdapter(new MyAdapter());
+  }
+
+  static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    @Override public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+      View view = inflater.inflate(R.layout.list_item, parent, false);
+
+      return new MyViewHolder(view);
     }
 
-    static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-
-        @Override
-        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            View view = inflater.inflate(R.layout.list_item, parent, false);
-
-            return new MyViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(MyViewHolder holder, int position) {
-            holder.textView.setText("Item " + position);
-        }
-
-        @Override
-        public int getItemCount() {
-            return 50;
-        }
-
-        public static class MyViewHolder extends RecyclerView.ViewHolder {
-            @BindView(R.id.text)
-            TextView textView;
-
-            public MyViewHolder(View itemView) {
-                super(itemView);
-                ButterKnife.bind(this, itemView);
-            }
-
-
-        }
+    @Override public void onBindViewHolder(MyViewHolder holder, int position) {
+      holder.textView.setText("Item " + position);
     }
+
+    @Override public int getItemCount() {
+      return 50;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+      @BindView(R.id.text) TextView textView;
+
+      public MyViewHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+      }
+    }
+  }
 }
